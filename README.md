46e547518cb332f8002eeee2f0328d819d621a49
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2022-03-25 19:24:16 -0500

    prompt skeleton

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Purpose
+A website to generate secure passwords with user chosen length and charecter types.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,34 @@
 // Assignment code here
+var generatePassword = function() {
 
+  // ask length
+  var passLength = window.prompt("What is your desired character length? Please choose a number between 8 and 128.");
+    if (!passLength) {
+      window.alert("Please enter a valid number.");
+      return generatePassword();
+    } else if (passLength < 8 || passLength > 128) {
+      alert("Please choose a number between 8 and 128.");
+      return generatePassword();
+    }
+  
+  // lowercase
+  var lowercase = window.confirm("Would you like lowercase letters?")
+
+  // uppercase
+  var uppercase = window.confirm("Would you like uppercase letters?")
+
+  // numerics
+  var num = window.confirm("Would you like numbers?")
+
+  // special characters
+  var specialChar = window.confirm("Would you like special characters?")
+
+    //must choose one criteria
+    if (!lowercase && !uppercase && !num && !specialChar) {
+      alert("You must choose at least one criteria.");
+      return generatePassword();
+    }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
